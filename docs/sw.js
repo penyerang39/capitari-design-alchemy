7673cc09fae6f15b38fd17f8fd5666c925ffd0c6
penyerang39 <boikafreewarrior@protonmail.com>
2025-05-14 19:44:28 +0300

    n

---
+++ docs/sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+  );
+});
