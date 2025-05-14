b1786dd730d15037d4f8459bd3edacb3b9067341
penyerang39 <boikafreewarrior@protonmail.com>
2025-05-14 19:42:53 +0300

    214215215

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/',
+  build: {
+    outDir: 'docs',
+  },
   server: {
     host: "::",
     port: 8080,
